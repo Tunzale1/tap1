383a41b4bc217d31d8111637e5f35dae9680a4dc
Tünzalə <114104944+Tunzale1@users.noreply.github.com>
2022-11-22 02:05:08 -0800

    Add files via upload
    111

---
+++ tap1/src/component/App/app.jsx
@@ -0,0 +1,9 @@
+const App = ()=>{
+    return (
+        <div>
+  <h3>Tunzala</h3>
+  <p>Now I can create component</p>
+</div>
+    )
+}
+ export default App
---
+++ tap1/src/component/Input/input.jsx
@@ -0,0 +1,8 @@
+import React,{Component} from 'react'
+
+class Input extends Component{
+  render(){
+    return(<p>Input</p>)
+  }
+}
+export default Input
